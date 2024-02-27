536ce3c39390a9cd4a8c3a1e482da03de38e6104
eMode18 <ewagandi@gmail.com>
2024-02-27 17:30:39 +0300

    trying to fix server errors on vercel deployed app

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const Logo = () => {
-  return <Image height={40} width={100} alt="logo" src="/logo.svg" />;
+  return (
+    <Link href="/">
+      <Image height={40} width={100} alt="logo" src="/logo.svg" />
+    </Link>
+  );
 };

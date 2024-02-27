176009fbd61e93eddc76d6104eaec52663e310ee
eMode18 <ewagandi@gmail.com>
2024-02-27 20:33:24 +0300

    fixed layout for clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ToasterProvider } from "@/components/providers/toaster-provider";
+import { ToastProvider } from "@/components/providers/toaster-provider";
 import { ConfettiProvider } from "@/components/providers/confetti-provider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,15 +14,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
           <ConfettiProvider />
-          <ToasterProvider />
+          <ToastProvider />
           {children}
         </body>
       </html>

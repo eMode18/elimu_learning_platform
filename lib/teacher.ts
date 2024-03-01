17712e899e4fc4c85ca28215b1f267ede71bd23b
eMode18 <ewagandi@gmail.com>
2024-03-01 10:11:31 +0300

    modified teacher access

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,3 +1,7 @@
 export const isTeacher = (userId?: string | null) => {
-  return userId === process.env.NEXT_PUBLIC_TEACHER_ID;
+  if (!userId) return false; // If userId is not provided, return false
+
+  const teacherIds = process.env.NEXT_PUBLIC_TEACHER_ID?.split(",") || []; // Split the environment variable value into an array of teacher IDs
+
+  return teacherIds.includes(userId); // Check if userId exists in the array of teacher IDs
 };
